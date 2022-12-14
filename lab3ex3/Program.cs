a17fa468c9d3b9a10e2a1c30d5562ac1d835c1d9
Alex Filip <alexfilippaul@gmail.com>
2022-12-14 16:24:11 +0200

    patrat perfect

---
+++ lab3ex3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace lab3ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Scrieti o functie care va determina daca un numar este sau nu patrat perfect.Apelati o si afisati i rezultatul
+            Console.WriteLine("Introduceti un numar: ");
+            int numar = int.Parse(Console.ReadLine());
+            PatratPerfect(numar);
+        }
+        static void PatratPerfect(int numar)
+        {
+            for(int i=1; i<=numar/2; i++)
+            {
+                if(i*i==numar)
+                {
+                    Console.WriteLine("Numarul este un patrat perfect");
+                    break;
+                }
+                else if(i==numar/2)
+                {
+                    Console.WriteLine("Numarul nu este un patrat perfect");
+                }
+            }
+            
+        }
+    }
+}
